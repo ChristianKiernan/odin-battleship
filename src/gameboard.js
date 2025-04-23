adf68aaa18556ac2c8430d761a2e62b91eb882be
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-04-23 12:39:22 -0400

    feat: Re-implement node class, and implement it in gameboard.js to represent each individual cell of the gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -7,13 +7,16 @@ export class Gameboard {
         this.board = [];
     }
     setBoard() {
-        for (let row = 0; row < this.rows; row++) {
-            board[i] = [];
-            for (let col = 0; col < this.columns; col++) {
-                let node = new Node();
-                board[i].push(node);
+        for (let i = 0; i < this.rows; i++) {
+            this.board[i] = [];
+            for (let j = 0; j < this.columns; j++) {
+                let node = new Node(null);
+                this.board[i][j] = node;
             }
         }
     }
+    placeShip(ship) {
+        const shipCords = ship.coordinates;
+        return this.board[shipCords[0]][shipCords[1]] = "full";
+    }
 }
-
