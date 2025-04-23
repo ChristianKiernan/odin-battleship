b0867e754b2ef267ad11b3a01b09e71a55d48807
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-04-23 08:05:56 -0400

    fix: Check to ensure jest config is working

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
 export function hit(val) {
     return val;
 }
+
+export function isSunk(val) {
+    return val;
+}
+
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,5 +1,12 @@
-import { hit } from "./utils.js"
+import { hit, isSunk } from "./utils.js"
 
+//hit tests
 test('returns an input', () => {
     expect(hit(1)).toEqual(1);
 });
+
+//isSunk tests
+test('returns an input', () => {
+    expect(hit(1)).toEqual(1);
+});
+
