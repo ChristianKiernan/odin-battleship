884a7a7a1a660b9b0224156c81ea3ad9422498c0
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-04-24 11:43:45 -0400

    feat: Add empty divs that will display player's guesses

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Controller } from "./controller";
 
 const newGame = new Controller();
 newGame.setUpOne();
+newGame.setUpTwo();
 setDomElements(newGame);
 
 function setDomElements(controllerObj) {
@@ -10,6 +11,15 @@ function setDomElements(controllerObj) {
     const containerDiv = document.createElement("div");
     containerDiv.classList.add("container");
     document.body.appendChild(containerDiv);
+
+    const playerOneDiv = document.createElement("div");
+    playerOneDiv.classList.add("guess-list");
+    playerOneDiv.setAttribute("id", "p1-list");
+    containerDiv.appendChild(playerOneDiv);
+
+    const playerOneHead = document.createElement("h2");
+    playerOneHead.innerHTML = "Coordinates Guessed:"
+    playerOneDiv.appendChild(playerOneHead);
 
     const boardOneDiv = document.createElement("div");
     boardOneDiv.classList.add("board");
@@ -20,6 +30,15 @@ function setDomElements(controllerObj) {
     messageBoard.classList.add("message");
     messageBoard.innerHTML = "Welcome to Battleship! Place Your Ships";
     containerDiv.appendChild(messageBoard);
+
+    const playerTwoDiv = document.createElement("div");
+    playerTwoDiv.classList.add("guess-list");
+    playerTwoDiv.setAttribute("id", "p2-list");
+    containerDiv.appendChild(playerTwoDiv);
+
+    const playerTwoHead = document.createElement("h2");
+    playerTwoHead.innerHTML = "Coordinates Guessed:"
+    playerTwoDiv.appendChild(playerTwoHead);
 
     const boardTwoDiv = document.createElement("div");
     boardTwoDiv.classList.add("board");
