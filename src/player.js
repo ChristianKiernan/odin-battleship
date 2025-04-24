00800d167411a51519a7330bcb6a991c80851a8f
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-04-24 17:31:21 -0400

    feat: Add guessList to player class

--- src/player.js
+++ src/player.js
@@ -4,5 +4,13 @@ export class Player {
     constructor(type) {
         this.type = type;
         this.board = new Gameboard();
+        this.guessList = [];
+    }
+    setGuessList(cords, type) {
+        const guess = {
+            Cords: cords,
+            Type: type
+        }
+        this.guessList.push(guess);
     }
 }
